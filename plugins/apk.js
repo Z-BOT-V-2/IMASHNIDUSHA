80604233dc6194bbb3fe319429825a7bdb54b891
Z-BOT-V-2 IMASH <91847406+Z-BOT-V-2@users.noreply.github.com>
2021-10-04 02:42:29 +0530

    Create apk.js

---
+++ plugins/apk.js
@@ -0,0 +1,291 @@
+const Asena = require('../events');
+const axios = require('axios');
+const {MessageType, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+const {spawnSync} = require('child_process');
+const Config = require('../config');
+const fs = require('fs');
+
+const Language = require('../language');
+const Lang = Language.getString('weather');
+
+
+if (Config.WORKTYPE == 'private') {
+
+   Asena.addCommand({pattern: 'apkmod', fromMe: true, desc: Lang.APK_MOD}, (async (message, match) => {
+      await message.sendMessage('┏━━━━━━━━━━━━━━━━━━━\n┃〘 🤡*Z BOT -  APK COMMANDS* 🤡〙\n┗━━━━━━━━━━━━━━━━━━━\nAplicaciones Full\n┠⊷️ 🤡 Nova Launcher:\n     *.nova*\n\n┠⊷️ 🤡 CM Launcher:\n     *.cml*\n\n┠⊷️ 🤡 Apex Launcher:\n     *.apex*\n\n\n┠⊷️ 🤡 Kinemaster:\n     *.kinemaster*\n\n┠⊷️ 🤡 PicsArt Gold:\n     *.picsart*\n\n┠⊷️ 🤡 Canva Pro:\n     *.canva*\n\n┠⊷️ 🤡 Ligthrom:\n     *.lightroom*\n\n┠⊷️ 🤡 Photoshop Express:\n     *.pshop*\n\n┠⊷️ 🤡 Snapseed:\n     *.snaps*\n\n┠⊷️ 🤡 Retouch:\n     *.retouch*\n\n\n┠⊷️ 🤡 Vanced Manager:\n     *.vanced*\n\n┠⊷️ 🤡 Crunchyroll:\n     *.crunchy*\n\n┠⊷️ 🤡 Freezer Mod:\n     *.freez*\n\n┠⊷️ 🤡 Deezer Premium:\n     *.deezer*\n\n┠⊷️ 🤡 RadioBox:\n     *.rbox*\n\n┠⊷️ 🤡 Mx Player Pro:\n     *.mxpro*\n\n┠⊷️ 🤡 Power AMP:\n     *.amp*\n\n┠⊷️ 🤡 JetAudio:\n     *.jetau*\n\n\n┠⊷️ 🤡 ExpressVpn:\n     *.xpress*\n\n┠⊷️ 🤡 Hospot Shield:\n     *.hshield*\n\n┠⊷️ 🤡 TurboVpn:\n     *.Turbo*\n\n┠⊷️ 🤡 File Manager:\n     *.flmanager*\n\n┠⊷️ 🤡 CallRecorder:\n     *.callr*\n\n┠⊷️ 🤡 FingScanner:\n     *.fing*\n\n┠⊷️ 🤡 Shazam Encore:\n     *.shazam*\n\n┠⊷️ 🤡 QR Scanner Pro:\n     *.qrcode*\n\n┠⊷️ 🤡 Screen Recorder:\n     *.srecorder*\n\n┠⊷️ 🤡 TikTok Mod:\n     *.tiktok*\n\n┠⊷️ 🤡 Photomath:\n     *.pmath*\n\n┠⊷️ 🤡 WhatsApp Plus:\n     *.waplus*\n\n┏━━━━━━━━━━━━━━━━━━━\n  *Create BY IMASH* 😉\n┗━━━━━━━━━━━━━━━━━━━\n');
+   }));
+
+   Asena.addCommand({pattern: 'nova', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *NOVA LAUNCHER* 🧲\npremium unlocked .\n⚡ bit.ly/drknova');
+   }));
+
+   Asena.addCommand({pattern: 'cml', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖*CM LAUNCHER* 🧲\npremium unlocked.\n😞 No disponible en este momento.');
+   }));
+
+   Asena.addCommand({pattern: 'apex', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *APEX LAUNCHER* 🧲\npremium unlocked.\n⚡ bit.ly/drkapex');
+   }));
+
+   Asena.addCommand({pattern: 'kinemaster', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *KINEMASTER MOD* 🧲\npremium unlocked.\n👉https://bit.ly/2RSyFVr.\n pass: 3456');
+   }));
+
+   Asena.addCommand({pattern: 'inshot', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *INSHOT MOD* 🧲\npremium unlocked.\n👉https://bit.ly/3zyNjlZ');
+   }));
+
+   Asena.addCommand({pattern: 'alight', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *ALIGHT MOTION MOD* 🧲\npremium unlocked.\n👉https://bit.ly/3cHUBdg\n pass: 3456');
+   }));
+
+   Asena.addCommand({pattern: 'capcut', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *CAP CUT MOD* 🧲\npremium unlocked.\n👉https://bit.ly/3pSSlFu');
+   }));
+   Asena.addCommand({pattern: 'picsart', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *PICSART GOLD* 🧲\npremium unlocked.\n⚡ bit.ly/drkpicsart');
+   }));
+
+   Asena.addCommand({pattern: 'canva', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *CANVA PRO* 🧲\npremium unlocked.\n⚡ bit.ly/canvapro');
+   }));
+
+   Asena.addCommand({pattern: 'lightr', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *LIGTHROM* 🧲\npremium unlocked.\n⚡ bit.ly/drklightr');
+   }));
+
+   Asena.addCommand({pattern: 'pshop', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *PHOTOSHOP EXPRESS* 🧲\npremium unlocked.\n⚡ bit.ly/drkphotoshop');
+   }));
+
+   Asena.addCommand({pattern: 'snaps', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *SNAPSEED* 🧲\npremium unlocked.\n⚡ bit.ly/drksnaps');
+   }));
+
+   Asena.addCommand({pattern: 'retouch', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *RETOUCH* 🧲\npremium unlocked.\n⚡ bit.ly/drkretouch');
+   }));
+
+   Asena.addCommand({pattern: 'vanced', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *VANCED MANAGER* 🧲\nyoutube vanced.\n⚡ bit.ly/drkytubev');
+   }));
+
+   Asena.addCommand({pattern: 'freez', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *FREEZER MOD* 🧲\nPremium Unlocked.\n⚡ bit.ly/drkfreezer');
+   }));
+
+   Asena.addCommand({pattern: 'deezer', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *DEEZER MOD* 🧲\npremium unlocked.\n⚡ bit.ly/drkdeezer');
+   }));
+
+   Asena.addCommand({pattern: 'rbox', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *RADIO BOX* 🧲\npremium unlocked.\n⚡ bit.ly/drkradiobox');
+   }));
+
+   Asena.addCommand({pattern: 'mxpro', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *MX PLAYER PRO* 🧲\npremium unlocked.\n⚠️ Versión Premium.\n⚡ bit.ly/drkmxplayer');
+   }));
+
+   Asena.addCommand({pattern: 'amp', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *POWERAMP* 🧲\npremium unlocked.\n⚡ bit.ly/drkampplayer');
+   }));
+
+   Asena.addCommand({pattern: 'jetau', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *JetAudio* 🧲\npremium unlocked.\n⚡ bit.ly/drkjetau');
+   }));
+
+   Asena.addCommand({pattern: 'xpress', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *EXPRESS VPN* 🧲\npremium unlocked.\n⚡ bit.ly/drkXpress');
+   }));
+
+   Asena.addCommand({pattern: 'hshield', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *HOSPOT SHIELD VPN* 🧲\npremium unlocked.\n⚡ bit.ly/drkHShield');
+   }));
+
+   Asena.addCommand({pattern: 'avguard', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *TURBO VPN* 🧲\npremium unlocked.\n⚡ bit.ly/drkguard');
+   }));
+
+   Asena.addCommand({pattern: 'flmanager', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *FILE MANAGER* 🧲\npremium unlocked.\n⚡ bit.ly/drkesfile');
+   }));
+
+   Asena.addCommand({pattern: 'callr', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *CALL RECORDER* 🧲\npremium unlocked.\n⚡ bit.ly/drkcallr');
+   }));
+
+   Asena.addCommand({pattern: 'fing', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *FING SCANNER* 🧲\npremium unlocked.\n⚡ bit.ly/drkfing');
+   }));
+
+   Asena.addCommand({pattern: 'shazam', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *SHAZAM ENCORE* 🧲\npremium unlocked.\n⚡ bit.ly/drkshaz');
+   }));
+
+   Asena.addCommand({pattern: 'qrcode', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *QR CODE SCANER* 🧲\npremium unlocked.\n⚡ bit.ly/drkqrscanner');
+   }));
+
+   Asena.addCommand({pattern: 'srecorder', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *SCREEN RECORDER* 🧲\npremium unlocked.\n⚡ bit.ly/drkgrabr');
+   }));
+
+   Asena.addCommand({pattern: 'tiktok', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *TIKTOK MOD* 🧲\npremium unlocked.\n⚡ bit.ly/drktiktok');
+   }));
+
+   Asena.addCommand({pattern: 'pmath', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *PHOTOMATH* 🧲\npremium unlocked.\n⚡ bit.ly/drkphotomath');
+   }));
+
+   Asena.addCommand({pattern: 'waplus', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *WHATSAPP PLUS* 🧲\nwhattsapp mod.\n⚡ bit.ly/drkWaPlus');
+   }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+   Asena.addCommand({pattern: 'apkmod', fromMe: false}, (async (message, match) => {
+      await message.sendMessage('┏━━━━━━━━━━━━━━━━━━━\n┃〘 🤡*Z BOT APK - COMANDOS* 🤡〙\n┗━━━━━━━━━━━━━━━━━━━\nAplicaciones Full\n┠⊷️ 🤡 Nova Launcher:\n     *.nova*\n\n┠⊷️ 🤡 CM Launcher:\n     *.cml*\n\n┠⊷️ 🤡 Apex Launcher:\n     *.apex*\n\n\n┠⊷️ 🤡 Kinemaster:\n     *.kinemaster*\n\n┠⊷️ 🤡 PicsArt Gold:\n     *.picsart*\n\n┠⊷️ 🤡 Canva Pro:\n     *.canva*\n\n┠⊷️ 🤡 Ligthrom:\n     *.lightroom*\n\n┠⊷️ 🤡 Photoshop Express:\n     *.pshop*\n\n┠⊷️ 🤡 Snapseed:\n     *.snaps*\n\n┠⊷️ 🤡 Retouch:\n     *.retouch*\n\n\n┠⊷️ 🤡 Vanced Manager:\n     *.vanced*\n\n┠⊷️ 🤡 Crunchyroll:\n     *.crunchy*\n\n┠⊷️ 🤡 Freezer Mod:\n     *.freez*\n\n┠⊷️ 🤡 Deezer Premium:\n     *.deezer*\n\n┠⊷️ 🤡 RadioBox:\n     *.rbox*\n\n┠⊷️ 🤡 Mx Player Pro:\n     *.mxpro*\n\n┠⊷️ 🤡 Power AMP:\n     *.amp*\n\n┠⊷️ 🤡 JetAudio:\n     *.jetau*\n\n\n┠⊷️ 🤡 ExpressVpn:\n     *.xpress*\n\n┠⊷️ 🤡 Hospot Shield:\n     *.hshield*\n\n┠⊷️ 🤡 TurboVpn:\n     *.Turbo*\n\n┠⊷️ 🤡 File Manager:\n     *.flmanager*\n\n┠⊷️ 🤡 CallRecorder:\n     *.callr*\n\n┠⊷️ 🤡 FingScanner:\n     *.fing*\n\n┠⊷️ 🤡 Shazam Encore:\n     *.shazam*\n\n┠⊷️ 🤡 QR Scanner Pro:\n     *.qrcode*\n\n┠⊷️ 🤡 Screen Recorder:\n     *.srecorder*\n\n┠⊷️ 🤡 TikTok Mod:\n     *.tiktok*\n\n┠⊷️ 🤡 Photomath:\n     *.pmath*\n\n┠⊷️ 🤡 WhatsApp Plus:\n     *.waplus*\n\n┏━━━━━━━━━━━━━━━━━━━\n  *Create BY IMASH* 😉\n┗━━━━━━━━━━━━━━━━━━━\n');
+   }));
+    
+   Asena.addCommand({pattern: 'fatp', fromMe: false}, (async (message, match) => {        
+        await message.client.sendMessage(
+            message.jid, 
+            fs.readFileSync("media/gif/apk/FATP.mp3"),
+            MessageType.audio, 
+            {mimetype: Mimetype.mp4Audio}
+        )
+   }));
+
+   Asena.addCommand({pattern: 'nova', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *NOVA LAUNCHER* 🧲\npremium unlocked.\n⚡ bit.ly/drknova');
+   }));
+
+   Asena.addCommand({pattern: 'cml', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *CM LAUNCHER* 🧲\npremium unlocked.\n😞 No disponible en este momento.');
+   }));
+
+   Asena.addCommand({pattern: 'apex', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *APEX LAUNCHER* 🧲\npremium unlocked.\n⚡ bit.ly/drkapex');
+   }));
+
+   Asena.addCommand({pattern: 'kinemaster', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *KINEMASTER MOD* 🧲\npremium unlocked.\n👉https://bit.ly/2RSyFVr.\npass: 3456');
+   }));
+
+Asena.addCommand({pattern: 'inshot', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *INSHOT MOD* 🧲\npremium unlocked.\n👉https://bit.ly/3zyNjlZ');
+   }));
+
+Asena.addCommand({pattern: 'alight', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *ALIGHT MOTION MOD* 🧲\npremium unlocked.\n👉https://bit.ly/3cHUBdg\n pass 3456');
+   }));
+
+Asena.addCommand({pattern: 'capcut', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *CAP CUT MOD* 🧲\npremium unlocked.\n👉https://bit.ly/3pSSlFu\npass 3456');
+   }));
+   Asena.addCommand({pattern: 'picsart', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *PICSART GOLD* 🧲\npremium unlocked.\n⚡ bit.ly/drkpicsart');
+   }));
+
+   Asena.addCommand({pattern: 'canva', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *CANVA PRO* 🧲\npremium unlocked.\n⚡ bit.ly/canvapro');
+   }));
+
+   Asena.addCommand({pattern: 'lightr', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *LIGTHROM* 🧲\npremium unlocked.\n⚡ bit.ly/drklightr');
+   }));
+
+   Asena.addCommand({pattern: 'pshop', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *PHOTOSHOP EXPRESS* 🧲\npremium unlocked.\n⚡ bit.ly/drkphotoshop');
+   }));
+
+   Asena.addCommand({pattern: 'snaps', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *SNAPSEED* 🧲\npremium unlocked.\n⚡ bit.ly/drksnaps');
+   }));
+
+   Asena.addCommand({pattern: 'retouch', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *RETOUCH* 🧲\npremium unlocked.\n⚡ bit.ly/drkretouch');
+   }));
+
+   Asena.addCommand({pattern: 'vanced', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *VANCED MANAGER* 🧲\nYouTube Premium  YouTube Music.\n⚡ bit.ly/drkytubev');
+   }));
+
+   Asena.addCommand({pattern: 'crunchy', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+        var chyroll = await axios.get('https://gitlab.com/DrkBotPublic/botarchives/-/raw/master/Sin_t%C3%ADtulo_2_540p.mp4', { responseType: 'arraybuffer' })
+        await message.client.sendMessage(message.jid, Buffer.from(chyroll.data), MessageType.video, {mimetype: Mimetype.mp4})
+        await message.sendMessage('🔰 *CRUNCHYROLL(vrv)* 🔰\n💳 *BIN:* 55703976xxx62xxx\n📫 *Codigo Postal:* 10080, 10090\n *Ip:* USA 🇺🇸');
+   }));
+
+   Asena.addCommand({pattern: 'freez', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *FREEZER MOD* 🧲\npremium unlocked - spotify\n⚡ bit.ly/drkfreezer');
+   }));
+
+   Asena.addCommand({pattern: 'deezer', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *DEEZER MOD* 🧲\npremium unlocked.\n⚡ bit.ly/drkdeezer');
+   }));
+
+   Asena.addCommand({pattern: 'rbox', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *RADIO BOX* 🧲\npremium unlocked.\n⚡ bit.ly/drkradiobox');
+   }));
+
+   Asena.addCommand({pattern: 'mxpro', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *MX PLAYER PRO* 🧲\npremium unlocked.\n⚡ bit.ly/drkmxplayer');
+   }));
+
+   Asena.addCommand({pattern: 'amp', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *POWERAMP* 🧲\npremium unlocked.\n⚡ bit.ly/drkampplayer');
+   }));
+
+
+   Asena.addCommand({pattern: 'xpress', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *EXPRESS VPN* 🧲\npremium unlocked.\n⚡ bit.ly/drkXpress');
+   }));
+
+   Asena.addCommand({pattern: 'hshield', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *HOSPOT SHIELD VPN* 🧲\npremium unlocked.\n⚡ bit.ly/drkHShield');
+   }));
+
+   Asena.addCommand({pattern: 'avguard', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *TURBO VPN* 🧲\npremium unlocked.\n⚡ bit.ly/drkguard');
+   }));
+
+   Asena.addCommand({pattern: 'flmanager', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *FILE MANAGER* 🧲\npremium unlocked.\n⚡ bit.ly/drkesfile');
+   }));
+
+   Asena.addCommand({pattern: 'callr', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *CALL RECORDER* 🧲\npremium unlocked.\n⚡ bit.ly/drkcallr');
+   }));
+
+   Asena.addCommand({pattern: 'fing', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *FING SCANNER* 🧲\npremium unlocked.\n⚡ bit.ly/drkfing');
+   }));
+
+   Asena.addCommand({pattern: 'shazam', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *SHAZAM ENCORE* 🧲\npremium unlocked.\n⚡ bit.ly/drkshaz');
+   }));
+
+   Asena.addCommand({pattern: 'qrcode', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *QR CODE SCANER* 🧲\npremium unlocked.\n⚡ bit.ly/drkqrscanner');
+   }));
+
+   Asena.addCommand({pattern: 'screen recorder', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *SCREEN RECORDER* 🧲\npremium unlocked.\n⚡ bit.ly/drkgrabr');
+   }));
+
+   Asena.addCommand({pattern: 'tiktok', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *TIKTOK MOD* 🧲\npremium unlocked.\n⚡ bit.ly/drktiktok');
+   }));
+
+   Asena.addCommand({pattern: 'pmath', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *PHOTOMATH* 🧲\npremium unlocked.\n⚡ bit.ly/drkphotomath');
+   }));
+
+   Asena.addCommand({pattern: 'waplus', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+      await message.sendMessage('🤖 *WHATSAPP PLUS* 🧲\nmhatsapp mod.\n⚡ bit.ly/drkWaPlus');
+   }));
+}
