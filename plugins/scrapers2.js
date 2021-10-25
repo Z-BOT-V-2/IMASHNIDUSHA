ba6e2ce74e871acbdcf8d0d3b1c1291acb9a4169
Z-BOT-V-2 IMASH <91847406+Z-BOT-V-2@users.noreply.github.com>
2021-10-26 01:23:12 +0530

    Update scrapers2.js

--- plugins/scrapers2.js
+++ plugins/scrapers2.js
@@ -263,12 +263,12 @@ else if (config.WORKTYPE == 'public') {
   });
 
     Asena.addCommand({pattern: 'bot ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
+	    if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
+	const url = `https://api.simsimi.net/v2/?text=${match[1]}&lc=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*baby🥰*' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*Lusifar tell* 💗' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
