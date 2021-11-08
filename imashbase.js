04d358babd9024cfce4b31355999ccc047bd6f4a
Z-BOT-V-2 IMASH <91847406+Z-BOT-V-2@users.noreply.github.com>
2021-11-09 00:41:53 +0530

    Update imashbase.js

--- imashbase.js
+++ imashbase.js
@@ -7,53 +7,34 @@ WhatsAsena - Yusuf Usta
 */
 
 const chalk = require('chalk');
-const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
+const {WAConnection, MessageOptions, MessageType, Mimetype} = require('@adiwajshing/baileys');
 const {StringSession} = require('./imashbase/');
 const fs = require('fs');
 
 async function whatsAsena () {
     const conn = new WAConnection();
     const Session = new StringSession();  
-    conn.version = [2, 2140, 12]
+    conn.version = [3, 3234, 9]
     conn.logger.level = 'warn';
     conn.regenerateQRIntervalMs = 50000;
-    
+
     conn.on('connecting', async () => {
         console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
 ${chalk.white.italic('AsenaString Kodu Alıcı')}
-
-${chalk.blue.italic('Z BOT CONNECTION YOUR WHATSAPP PLEASE WAIT⛛')}`);
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
     });
-    
-
     conn.on('open', async () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
-            chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+            chalk.green.bold(conn.user.jid.startsWith('90') || conn.user.jid.startsWith('994') ? 'WhatsAsena String Kodunuz: ' : 'WhatsAsena String Code: '), st
         );
-        
+
         if (!fs.existsSync('config.env')) {
             fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
         }
-        if (conn.user.jid.startsWith('90')) {
-            await conn.sendMessage(conn.user.jid,st, MessageType.text)
-            await conn.sendMessage(conn.user.jid,'*Bu Kodu Kimseyle Paylaşmayın!*', MessageType.text)
-            console.log(
-                chalk.blue.bold('Locale kuruyorsanız node bot.js ile botu başlatabilirsiniz.')
-            );
-        }
-        else {
-            await conn.sendMessage(conn.user.jid,st, MessageType.text)
-            await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!*', MessageType.text)
-            console.log(
-                chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js')
-            );
-        }
-        
+        console.log(conn.user.jid.startsWith('90') || conn.user.jid.startsWith('994') ? 'Bu Kodu Kimseyle Paylaşma ' + conn.user.name : 'Dont Share This Code to Anyone ' + conn.user.name)
         process.exit(0);
     });
-
     await conn.connect();
 }
-
 whatsAsena()
