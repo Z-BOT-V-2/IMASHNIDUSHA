313aa9e8375628d0bd58f42f4f91739fe73a076c
Z-BOT-V-2 IMASH <91847406+Z-BOT-V-2@users.noreply.github.com>
2021-11-11 03:03:50 +0530

    Update skl.js

--- plugins/skl.js
+++ plugins/skl.js
@@ -216,3 +216,50 @@ Asena.addCommand(
     });
   }
 );
+Asena.addCommand(
+
+  {
+
+    pattern: "fb ?(.*)",
+
+    fromMe: true,
+
+    desc: Lang.FB_DESC,
+
+  },
+
+  async (message, match) => {
+
+    match = !message.reply_message ? match : message.reply_message.text;
+
+    if (match === "") return await message.sendMessage(Lang.NEED_REPLY);
+
+    await message.sendMessage(Lang.DOWNLOADING);
+
+    let links = await downVideo(match);
+
+    if (links.length == 0) return await message.sendMessage(Lang.NOT_FOUND);
+
+    let { buffer, size } = await getBuffer(links[0]);
+
+    if (size > 100)
+
+      return await message.sendMessage(
+
+        Lang.SIZE.format(size, links[0], links[1])
+
+      );
+
+    return await message.sendMessage(
+
+      buffer,
+
+      { quoted: message.quoted, caption: Lang.CAPTION.format(links[1] || "") },
+
+      MessageType.video
+
+    );
+
+  }
+
+);
