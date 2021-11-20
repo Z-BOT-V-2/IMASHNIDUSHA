5f1b968ccd9b2f723b7fec86fd183cf4a0207d63
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-11-20 17:22:10 +0530

    mistake fixed🤪

--- plugins/yt.js
+++ plugins/yt.js
@@ -40,7 +40,7 @@ const { BYE_LOGO } = require('../config');
 let treue = Config.WORKTYPE == 'public' ? false : true
 //=====================================================================================
 
- Asena.addCommand({pattern: 'yt ?(.*)', fromMe: false,  deleteCommand: false, desc: Lang.YT_DESC}, (async (message, match) => { 
+ Asena.addCommand({pattern: 'yt ?(.*)', fromMe: treue,  deleteCommand: false, desc: Lang.YT_DESC}, (async (message, match) => { 
 
     if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);    
     var searching = await message.client.sendMessage(message.jid,Lang.GETTING_VIDEOS,MessageType.text, {quoted: message.data});
@@ -60,22 +60,4 @@ let treue = Config.WORKTYPE == 'public' ? false : true
     return await message.client.deleteMessage(message.jid, {id: searching.key.id, remoteJid: message.jid, fromMe: true})
 }));
 
-Asena.addCommand({pattern: 'yt ?(.*)', fromMe: true,  deleteCommand: false, desc: Lang.YT_DESC}, (async (message, match) => { 
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);    
-    var searching = await message.client.sendMessage(message.jid,Lang.GETTING_VIDEOS,MessageType.text, {quoted: message.data});
-
-    try {
-        var arama = await yts(match[1]);
-    } catch {
-        return await message.client.sendMessage(message.jid,Lang.NOT_FOUND,MessageType.text);
-    }
-    
-    var ytgot = '';
-    arama.all.map((video) => {
-        ytgot += '😎 *' + video.title + '*' + '\n' + '*🤡Duration:-* ' +  video.duration +  '\n' + '*📎Link:-* ' + video.url + '\n'+ '*📅time ago:-* ' + video.ago + '\n\n'
-    });
-
-    await message.client.sendMessage(message.jid, '*Z YOUTUBE VIDEO SEARCH🧲*\n' + 'Result ...' + '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n' + ytgot,MessageType.text, {quoted: message.data});
-    return await message.client.deleteMessage(message.jid, {id: searching.key.id, remoteJid: message.jid, fromMe: true})
-}));
