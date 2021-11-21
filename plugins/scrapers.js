71dd74aafb13c97a26338823b1aa717c3cfcb1b6
Z-BOT-V-2 IMASH <91847406+Z-BOT-V-2@users.noreply.github.com>
2021-11-21 09:17:54 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -964,50 +964,31 @@ else if (config.WORKTYPE == 'public') {
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio,contextInfo: { forwardingScore: 2, isForwarded: true }, quoted: message.data, ptt: false});
             });
     }));
-    Asena.addCommand({pattern: 'video ?(.*)', fromMe: true, desc: Lang.VIDEO_DESC}, (async (message, match) => { 
+    Asena.addCommand({pattern: 'video ?(.*)', fromMe: false, desc: Lang.VIDEO_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_VIDEO,MessageType.text);    
-
     
-
         var VID = '';
-
         try {
-
             if (match[1].includes('watch')) {
-
                 var tsts = match[1].replace('watch?v=', '')
-
                 var alal = tsts.split('/')[3]
-
                 VID = alal
-
             } else {     
-
                 VID = match[1].split('/')[3]
-
             }
-
         } catch {
-
             return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
-
         }
-
         var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_VIDEO,MessageType.text);
 
         var yt = ytdl(VID, {filter: format => format.container === 'mp4' && ['1080p','720p', '480p', '360p', '240p', '144p'].map(() => true)});
-
         yt.pipe(fs.createWriteStream('./' + VID + '.mp4'));
 
         yt.on('end', async () => {
-
             reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, { caption: "UPLOADING Z BOT" });
-
+            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, { quoted: message.data, caption: "සෑ වෑව් ලස්සන වීඩියෝ කෑල්ල🙊\nᴘᴏᴡᴇʀᴅ ʙʏ ᴢʙᴏᴛ" });
         });
-
     }));
     
 
