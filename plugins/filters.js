f08d1a7b775eef66cd369f6623353abbd0a43be6
Z-BOT-V-2 IMASH <91847406+Z-BOT-V-2@users.noreply.github.com>
2021-11-22 02:33:12 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -215,9 +215,9 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
         let banned = jid.find( Jid => Jid === message.jid);
         if(banned !== undefined) return
         if (!!message.mention && message.mention[0] == '94771323434@s.whatsapp.net') {
-await message.client.sendMessage(message.jid, fs.readFileSync('./White/video/Whitedevil.mp4'), MessageType.video, { mimetype: Mimetype.mp4Video, quoted : message.data, ptt: true})
+await message.client.sendMessage(message.jid, fs.readFileSync('./imashmusic/menu.mp4'), MessageType.video, { mimetype: Mimetype.mp4Video, quoted : message.data, ptt: true})
         }
-const array = ['imash','Imash'',menu']
+const array = ['imash','Imash'',menu','alive']
 
 array.map( async (a) => {
 
@@ -225,7 +225,7 @@ let pattern = new RegExp(`\\b${a}\\b`, 'g');
 
 if(pattern.test(message.message)){
 
-       await message.client.sendMessage(message.jid, fs.readFileSync('./White/video/' + a + '.mp4'), MessageType.video, { mimetype: Mimetype.mp4Video, quoted: message.data, ptt: true})
+       await message.client.sendMessage(message.jid, fs.readFileSync('./imashmusic/' + a + '.mp4'), MessageType.video, { mimetype: Mimetype.mp4Video, quoted: message.data, ptt: true})
 
 }
 
